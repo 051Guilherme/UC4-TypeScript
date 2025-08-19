b502366bc8e959e234e1c89407f11369dafb145f
051Guilherme <guilherme.pereirasci@gmail.com>
2025-08-18 21:45:55 -0300

    arq

--- Dia-18-08/src/index.ts
+++ Dia-18-08/src/index.ts
@@ -1,3 +1,5 @@
+import { Animal } from "./Animal";
+import { Crocodile } from "./Crocodile";
 import { Dog } from "./Dog";
 import { Owl } from "./Owl";
 
@@ -11,3 +13,14 @@ const myOwl:Owl = new Owl('Chica', 15)
 console.log(myOwl.name, myOwl.weigth)
 myOwl.eat()
 myOwl.fly()
+
+const myCrocodile:Crocodile = new Crocodile('JeffBala', 30)
+console.log(myCrocodile.name, myCrocodile.weigth)
+myCrocodile.eat()
+myCrocodile.swim()
+
+const animais:Animal[] = [myCrocodile, myDog, myOwl];
+
+for (let i = 0; i < animais.length; i++) {
+    animais[i].eat();
+  }
