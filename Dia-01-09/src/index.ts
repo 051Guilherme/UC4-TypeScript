f38eb944fde7017ac9ebd4e6be9887f7a6b97ae1
051Guilherme <guilherme.pereirasci@gmail.com>
2025-09-04 20:10:29 -0300

    atualizando

--- Dia-01-09/src/index.ts
+++ Dia-01-09/src/index.ts
@@ -38,3 +38,20 @@ if (cliente.autenticar("guisci", "1234")) {
 } else {
   console.log("Falha na autenticação do cliente");
 }
+
+import { ClaAkimichi } from "./ClaAkimichi";
+import { ClaHyuuga } from "./ClaHyuuga";
+import { ClaNara } from "./ClaNara";
+import { ClaUchiha } from "./ClaUchiha";
+
+const personagemUchiha:ClaUchiha = new ClaUchiha("Uchiha", "Sasuke")
+
+const personagemHyuuga:ClaHyuuga = new ClaHyuuga("Hyuuga", "Hiashi")
+
+const personagemNara:ClaNara = new ClaNara("Nara", "Shikamaru")
+
+const personagemAkimichi:ClaAkimichi = new ClaAkimichi("Akimichi", "Chouza")
+
+personagemUchiha.habilidadeEspecial()
+
+personagemHyuuga.ataqueEspecial()
